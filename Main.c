64a5a63f11b3ff3bef6fb7972026ec53f010576f
Adam Pontin <arp530@its.york.ac.uk>
2017-02-07 16:22:57 +0000

    Main.c cleaned up

--- Main.c
+++ Main.c
@@ -13,68 +13,13 @@
 // replace Delay with osDelay for compatibility with RTOS
 #define Delay osDelay
 
-/*----------------------------------------------------------------------------
-  MAIN function
- *----------------------------------------------------------------------------*/
-int main (void) {
-	
-	osKernelInitialize();     
 
-  HAL_Init();           
-
-  SystemClock_Config();
-
-  LED_Init();  
-  SWT_Init();	
-	ADC1_Init();
-	
-	
-  LCD_Initpins();	
-	LCD_DriverOn();
-	Delay(10);
-	LCD_Init();
-	LCD_DriverOn();
-	LCD_On(1);
-
+void testing (void)
+{
 	uint32_t btns = 0;
 	uint32_t leds = 0;
-	uint32_t value = 0;
-	double value_calk = 0;
-	char string[17];
-	
-  SystemCoreClockUpdate();                      /* Get Core Clock Frequency   */
-  if (SysTick_Config(SystemCoreClock / 1000)) { /* SysTick 1 msec interrupts  */
-    while (1);                                  /* Capture error              */
-  }
-
-	GPIOD->ODR = 0;																/* turn LEDs off */
-	/*while (1) {
-		GPIOD->ODR = 1UL << 8;
-		Delay(1000);
-		GPIOD->ODR = 0;
-		Delay(1000);
-	}*/
-	
-	while (1) {
-		value = read_ADC1();
-		value = (value *16);
-		GPIOD->ODR = value;
-		
-		value_calk = ((double)value / (pow(2.0, 16.0))) * 3.3;
-		//value_calk = (double)0.5;
-		
-		LCD_Clear();
-		LCD_GotoXY(0,0);
-		sprintf(string, "                ");
-		sprintf(string, "%1.9lf", value_calk);
-		LCD_PutS(string);
-		LCD_GotoXY(15,0);
-		LCD_PutS("V");
-		Delay(100);
-	}
-
-  while(1) {                                    /* Loop forever               */
-    btns = SWT_Debounce();                           /* Read switch states         */
+	while(1) {                                    /* Loop forever               */
+		btns = SWT_Debounce();                           /* Read switch states         */
 		if (btns != 0)
 		{
 			leds = leds ^ btns; 
@@ -121,5 +66,61 @@ int main (void) {
 				
 			}
 		}
+	}
+}
+
+/*----------------------------------------------------------------------------
+  MAIN function
+ *----------------------------------------------------------------------------*/
+int main (void) {
+	
+	osKernelInitialize();     
+
+  HAL_Init();           
+
+  SystemClock_Config();
+
+  LED_Init();  
+  SWT_Init();	
+	ADC1_Init();
+	
+	
+  LCD_Initpins();	
+	LCD_DriverOn();
+	Delay(10);
+	LCD_Init();
+	LCD_DriverOn();
+	LCD_On(1);
+
+	
+	uint32_t value = 0;
+	double value_calk = 0;
+	char string[17];
+	
+  SystemCoreClockUpdate();                      /* Get Core Clock Frequency   */
+  if (SysTick_Config(SystemCoreClock / 1000)) { /* SysTick 1 msec interrupts  */
+    while (1);                                  /* Capture error              */
+  }
+
+	GPIOD->ODR = 0;																/* turn LEDs off */
+
+	//testing();
+	
+	while (1) {
+		value = read_ADC1();
+		value = (value *16);
+		GPIOD->ODR = value;
+		
+		value_calk = ((double)value / (pow(2.0, 16.0))) * 3.3;
+		//value_calk = (double)0.5;
+		
+		LCD_Clear();
+		LCD_GotoXY(0,0);
+		sprintf(string, "                ");
+		sprintf(string, "%1.9lf", value_calk);
+		LCD_PutS(string);
+		LCD_GotoXY(15,0);
+		LCD_PutS("V");
+		Delay(100);
 	}	
 }
