b4d6887cfd17ac429e0835804701d0de0588333c
Haroom Omodudu <ho552@its.york.ac.uk>
2017-02-21 15:29:59 +0000

    Updated range switching

--- Calculations.c
+++ Calculations.c
@@ -2,57 +2,78 @@
 #include <math.h>
 #include "STM32F4xx.h"
 #include "cmsis_os.h"
+#include "LED.h"
 
 /* Utility class to perform mathematical operations such as ADC conversion */
 
+
+// Define function prototypes
+double switchRange(int value, int *rangep, double scale);
+double adcConv(int mode, double value, int *rangep);
+
+
+
 double adcConv(int mode, double value, int *rangep){
-	
-	int range = *rangep;
+
 	double output;	// MODE dependant digital output. 
 	double inputCurrent = 3*pow(10,-6); // 3 microamp system input current - constant defined by hardware team
-	int maxRange = 1;
-	int minRange = 0;
+
 	
 	// CURRENT MODE - input value ranges from [0 to 3], output ranges from [-1 to 1]
 	if (mode ==	0){ 
-		if(range == 0){
-		output = (((double)value / (pow(2.0, 16.0)) * 3.3)-1.5) / 1.5;	
-		}
-		if(range == 1){
-		output = (((double)value / (pow(2.0, 16.0)) * 3.3)-1.5) / 15;	
-		}	
-			
-		if ((output > -0.1) & (output < 0.1)){
-			if (range < maxRange) {
-				range++;
-			}
-			else {
-				// TODO: Print error to LCD
-			}
-		}
-		else {
-			if (range > minRange) {
-				range--;
-			}
-			else {
-				// TODO: Print error to LCD
-			}
-		}
+			LED_Out(1);
+			output = switchRange(value, rangep, 0.66);
 	}
 	
-	// VOLTAGE MODE input - value ranges from [0 to 0], output ranges from [-10 to 10]
+	// VOLTAGE MODE input - value ranges from [0 to 3], output ranges from [-10 to 10]
 	if (mode ==	1){ 
-		output = (((double)value / (pow(2.0, 16.0)) * 3.3)-1.5) * 6.66;	
+		LED_Out(2);
+		output = switchRange(value, rangep, 6.66);
 	} 
 	
 	// RESISTANCE MODE - Use ohms law to calculate R. 
 	if (mode ==	2){ 
-		output = ((double)value / (pow(2.0, 16.0)) * 3.3) / inputCurrent;		
+		LED_Out(4);
+		output = switchRange(value, rangep, 6.66) / inputCurrent;		
 	} 	
 
-	*rangep = range;
-	return output;
-	
-			
+	return output;		
 	
 }
+
+double switchRange(int value, int *rangep, double scale){
+	
+	int maxRange = 1;
+	int minRange = 0;
+	double output;
+	
+	if(*rangep == 0){
+		output = (((double)value / (pow(2.0, 16.0)) * 3.3)-1.5) * scale;	
+		}
+	if(*rangep == 1){
+		output = (((double)value / (pow(2.0, 16.0)) * 3.3)-1.5) * scale*0.1000;	
+		}
+	
+	if ((output > -0.1) && (output < 0.1)){
+ 
+		if (*rangep < maxRange) {
+				*rangep = 1;
+		}
+		else {
+				// TODO: Print error to LCD
+		}
+	}
+	else {
+	
+		if (*rangep > minRange) {
+			*rangep = 0;
+		}
+		else {
+				// TODO: Print error to LCD
+		}
+	}
+	
+	return output;
+}
+
+
