58e9fa36a53c600702b3256f61678ea299a192a8
Haroom Omodudu <ho552@its.york.ac.uk>
2017-04-18 15:34:24 +0100

    Added dependancies

--- Calculations.h
+++ Calculations.h
@@ -2,5 +2,7 @@
 #define __CALCULATIONS_H
 
 extern double adcConv(int mode, double value, int *range);
+extern double movAvg(double avgIn, int mode, int *rangep);
+void setTimerValue(int timer);
 
 #endif
